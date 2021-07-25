15392c1dee1de56aabde47a5a8fca635d7510c56
PanFengli <1159929061@qq.com>
2021-07-25 14:43:16 +0800

    feat:合并完成，加上了侧边栏和坐标图

--- app/src/main/java/com/dalab/dalabapp/MainPage.java
+++ app/src/main/java/com/dalab/dalabapp/MainPage.java
@@ -3,11 +3,18 @@ package com.dalab.dalabapp;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.dalab.dalabapp.Adapter.ListAdapter;
 import com.dalab.dalabapp.Animation.Animation;
@@ -17,11 +24,26 @@ import com.dalab.dalabapp.Trains.Hemostasis;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainPage extends AppCompatActivity {
+    DrawerLayout mDrawerLayout;
+    Toolbar toolbar;
+    LinearLayout mDrawerContent;
+    EditText lowerBound;
+    EditText upperBound;
+    int lowerValue = 200;
+    int upperValue = 600;
+
     ListView mylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_page);
+
+        mDrawerLayout=findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu);
+        setSupportActionBar(toolbar);//用我们自定义的toolbar
+
         mylist = findViewById(R.id.list);
         mylist.setAdapter(new ListAdapter(MainPage.this));
         mylist.setOnItemClickListener(
@@ -38,6 +60,21 @@ public class MainPage extends AppCompatActivity {
                     }
                 }
         );
+
+        mDrawerContent=findViewById(R.id.right_drawer);
+
+        lowerBound = findViewById(R.id.lowerBound);
+        upperBound = findViewById(R.id.upperBound);
+        lowerBound.setText(String.valueOf(lowerValue));
+        upperBound.setText(String.valueOf(upperValue));
+        setFoucus(lowerBound);
+        setFoucus(upperBound);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);//填充菜单
+        //这样就可以保证不会出问题！
+        return true;
     }
     private class MyOnItemClickListener implements AdapterView.OnItemClickListener {
         @Override
@@ -55,5 +92,56 @@ public class MainPage extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+    }
+    private void setFoucus(final EditText et)
+    {
+        et.setOnFocusChangeListener(new android.view.View.
+                OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    // 此处为得到焦点时的处理内容
+                } else {
+                    // 此处为失去焦点时的处理内容
+                    if (lowerBound.getText() != null && !lowerBound.getText().toString().equals("")) {
+                        lowerValue = Integer.parseInt(lowerBound.getText().toString());
+                        lowerBound.setText(String.valueOf(lowerValue));
+                    } else {
+                        lowerValue = 0;
+                        lowerBound.setText("");
+                    }
+                    if (upperBound.getText() != null && !upperBound.getText().toString().equals("")) {
+                        upperValue = Integer.parseInt(upperBound.getText().toString());
+                        upperBound.setText(String.valueOf(upperValue));
+                    } else {
+                        upperValue = 1;
+                        upperBound.setText("");
+
+                    }
+
+                    if (lowerValue > upperValue || lowerValue == upperValue) {
+                        upperValue = lowerValue + 1;
+                        upperBound.setText(String.valueOf(upperValue));
+                    }
+
+                }
+            }
+        });
+    }
+    public void openSettings(View view)
+    {
+        //这是一个按钮的函数，用来打开侧边栏。之后甚至还可以设置设计一个返回按钮来关闭侧边栏
+        mDrawerLayout.openDrawer(mDrawerContent);
+    }
+
+    public void MenuOpenSettings(MenuItem menuItem)
+    {
+        //这是一个按钮的函数，用来打开侧边栏。之后甚至还可以设置设计一个返回按钮来关闭侧边栏
+        mDrawerLayout.openDrawer(mDrawerContent);
+    }
+    public void closeSettings(View view)
+    {
+        //这是一个按钮的函数，用来打开侧边栏。之后甚至还可以设置设计一个返回按钮来关闭侧边栏
+        mDrawerLayout.closeDrawer(mDrawerContent);
     }
 }
--- app/src/main/java/com/dalab/dalabapp/TrainingPages/TrainingHomeostasis.java
+++ app/src/main/java/com/dalab/dalabapp/TrainingPages/TrainingHomeostasis.java
@@ -2,15 +2,21 @@ package com.dalab.dalabapp.TrainingPages;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dalab.dalabapp.MainPage;
 import com.dalab.dalabapp.R;
+import com.dalab.dalabapp.SelfDefineViews.DrawLineChart;
 
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -24,10 +30,23 @@ public class TrainingHomeostasis extends AppCompatActivity {
     TextView timerText;
     Timer timer1;
     Button button;
+
+    ArrayList<Float> Values = new ArrayList<Float>();
+    int sampleDistance = 5;//采样的间距，在demo里面体现为每点击n次按钮才会显示一次
+    ArrayList<Float> storage = new ArrayList<Float>();
+//    EditText lowerBound;
+//    EditText upperBound;
+//    int lowerValue = 200;
+//    int upperValue = 600;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualization);
+
+        init();
+        Values.add(0.0f);
+
         body = findViewById(R.id.body);
         bound = findViewById(R.id.blood_bound_upper_left);
         wound = findViewById(R.id.blood_upper_left);
@@ -55,6 +74,7 @@ public class TrainingHomeostasis extends AppCompatActivity {
                     @Override
                     public void run() {
                         count += 10;
+                        changeWithSample();
 //                        String tmp = getStringTime(count);
                         timerText.setText(getStringTime(count));
                         if(count >= 5000)
@@ -81,5 +101,58 @@ public class TrainingHomeostasis extends AppCompatActivity {
         int second=cnt%60000/1000;
         int minisecond=cnt%1000/10;
         return String.format(Locale.CHINA,"%02d:%02d:%02d",min,second,minisecond);
+    }
+    private void init() {
+//        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.layout);//获取帧布局管理器，自己新建一个就可以了
+
+
+        DrawLineChart chart = findViewById(R.id.chart);
+        chart.setBrokenLineLTRB(50, 15, 10, 5);
+        chart.setRadius(2.5f);
+        chart.setCircleWidth(1f);
+        chart.setBorderTextSize(15);//修改边框文字大小
+        chart.setBrokenLineTextSize(10);//修改这线上文字大小
+        chart.setMaxVlaue(600);
+        chart.setMinValue(0);
+        chart.setNumberLine(5);//5根线
+        chart.setBorderWidth(1f);
+        chart.setBrokenLineWidth(1.5f);
+        chart.setBorderTransverseLineWidth(1.0f);//中间横线的宽度
+        chart.setUpper(600.0f);
+        chart.setLower(200.0f);
+
+        Random random = new Random();
+        float[] floats = new float[24];
+        for (int i = 0; i < floats.length; i++) {
+            float f = random.nextFloat();
+            floats[i] = f * 60 - 10;
+            Log.i("onCreate", "onCreate: f" + f);
+        }
+        chart.setValue(floats);
+    }
+
+    private void changeWithSample() {
+        DrawLineChart chart = findViewById(R.id.chart);
+        Random random = new Random();
+        float f = random.nextFloat();
+        float data = f * 600;//这个是生成的data，之后当然是用传递过来的数据
+        storage.add(data);
+        if (storage.size() == sampleDistance) {
+            float average = 0;
+            for (final Float value : storage) {
+                average += value;
+            }
+            average /= sampleDistance;
+            System.out.println("average：" + average);
+            Values.add(average);
+            float[] floats = new float[Values.size()];
+            int index = 0;
+            for (final Float value : Values) {
+                floats[index++] = value;
+            }
+            chart.setValue(floats);
+            chart.invalidate();//重绘
+            storage.clear();
+        }
     }
 }
