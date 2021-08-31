d5bcb399604237ae481e4da0710063c0f6f22505
徐珺涵 <XJHlalala@sjtu.edu.cn>
2021-08-31 15:24:57 +0800

    :tiger:

--- app/src/main/java/com/dalab/dalabapp/Utils/HoeoIncreaseModel.java
+++ app/src/main/java/com/dalab/dalabapp/Utils/HoeoIncreaseModel.java
@@ -5,6 +5,7 @@ import com.dalab.dalabapp.constant.Global;
 //此工具类用来保存、计算开始的时间，计算有效时间与失血量
 public class HoeoIncreaseModel {
     public int percent = 100;
+    final int _maxLose = 4000;
     int max;
     int min;
     int volumn;
@@ -38,6 +39,8 @@ public class HoeoIncreaseModel {
                 if(!recover)
                 {
                     lose += bloodSpeed*0.001f*speed;//这里的bleedspeed是按照s为单位的，每次update之间的时间间隔是speed，单位是ms，
+                    if(lose > _maxLose)
+                        lose = _maxLose;
                     percent = Math.max((volumn - (int)lose) * 100 / volumn, 0);
                 }
                 judgeGap = 0;
@@ -68,6 +71,8 @@ public class HoeoIncreaseModel {
                 if(!recover)
                 {
                     lose += bloodSpeed*0.001f*speed;
+                    if(lose > _maxLose)
+                        lose = _maxLose;
                     percent = Math.max((volumn - (int)lose) * 100 / volumn, 0);
                 }
             }
